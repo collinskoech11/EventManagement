d72600dae7828479bb3d343e5888e457222c9223
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-07-01 08:12:59 +0300

    set up form views

--- event/src/Pages/Manager/CreateEvent.js
+++ event/src/Pages/Manager/CreateEvent.js
@@ -5,7 +5,8 @@ function CreateEvent() {
   return (
     <div className="mydiv">
       <Form>
-      <Form.Group className="mb-3">
+        <Form.Group className="mb-3">
+
           <Form.Label>Event title</Form.Label>
           <Form.Control type="text" placeholder="Cocktail Party" />
           <Form.Text className="text-muted">
@@ -15,9 +16,41 @@ function CreateEvent() {
 
         <Form.Group className="mb-3">
           <Form.Label>Event Venue</Form.Label>
-          <Form.Control type="text" placeholder="Enter Event Venue" />
+          <Form.Control type="text" placeholder="Nairobi" />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Event Category</Form.Label>
+          <Form.Control type="text" placeholder="Friendly Meet Up" />
+          <Form.Text className="text-muted">
+           DEscribe what goes on at the event
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Event Description</Form.Label>
+          <Form.Control type="text" placeholder="Friendly Meet Up" />
+          <Form.Text className="text-muted">
+           Whats the Big Idea
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Event Type</Form.Label>
+          <Form.Control type="text" placeholder="Virtual or Physical" />
+          <Form.Text className="text-muted">
+          Whats the Big Idea
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Event Date</Form.Label>
+          <Form.Control type="date" placeholder="Friendly Meet Up" />
+          <Form.Text className="text-muted"> 
+          When is it happening
           </Form.Text>
         </Form.Group>
 
@@ -25,9 +58,12 @@ function CreateEvent() {
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
+
+
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+          <Form.Check type="checkbox" label="Get future notifications" />
         </Form.Group>
+
         <Button variant="primary" type="submit">
           Submit
         </Button>
--- event/src/Pages/User/SubmitRsvp.js
+++ event/src/Pages/User/SubmitRsvp.js
@@ -1,9 +1,52 @@
 import React from 'react'
+import {Form, Group, Label, Control, Check, Button} from 'react-bootstrap'
+
 
 function SubmitRsvp() {
   return (
-    <div>
-      Submit Rsvp
+    <div className="mydiv">
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Your Name</Form.Label>
+          <Form.Control type="text" placeholder="John Doe" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="elonmusk@tesla.com" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Event Name</Form.Label>
+          <Form.Control type="text" placeholder="Friendly Meet Up" />
+          <Form.Text className="text-muted">
+           Describe what goes on at the event
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Your Company Name</Form.Label>
+          <Form.Control type="text" placeholder="Tesla" />
+          <Form.Text className="text-muted">
+          Whats the Big Idea
+          </Form.Text>
+        </Form.Group>
+
+
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Get future notifications" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   )
 }
