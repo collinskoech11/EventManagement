5f5e584439da7d4767d07e1fe634794ca75dd054
collinskoech11 <collinskoechck34@gmail.com>
2022-07-28 14:31:05 +0300

    Event test

--- Backend/event/tests.py
+++ Backend/event/tests.py
@@ -5,26 +5,26 @@ from .models import *
 class MyAppUserTestCase(TestCase):
     def setUp(self):
         sampleUser = AppUser.objects.create(
-            username="sample",
+            username="John Doe",
             email="sample@gmail.com",
             role="Manager",
             Company="sample_logistics",
             password="password",
             location="Nairobi"
         )
-        # sampleEvent = Event.objects.create(
-        #     title="meet up",
-        #     # banner=,
-        #     Venue="Nairobi",
-        #     category="meet",
-        #     description="meet up",
-        #     type="physical",
-        #     created_at=2022-12-12,
-        #     date=2022-12-12,
-        #     time_created=2022-12-12
-        #     )
+        sampleEvent = Event.objects.create(
+            title="Link up",
+            # banner="sample.png",
+            Venue="Nairobi",
+            category="meet",
+            description="meet up",
+            type="physical",
+            created_at="2022-12-12",
+            date="2022-12-12",
+            time_created="2022-12-12"
+            )
         sampleRsvp = Rsvp.objects.create(
-            name="sample",
+            name="Faith",
             email="sample@gmail.com",
             event="meet up",
             Company="sample_logistics",
@@ -34,8 +34,13 @@ class MyAppUserTestCase(TestCase):
         sampleRsvp.save()
     def testAppUserModel(self):
         print("Testing App User Model")
-        sampleUserTest = AppUser.objects.get(username="sample")
-        print(sampleUserTest.username)
+        sampleUserTest = AppUser.objects.get(username="John Doe")
+        print("Sample User => ", sampleUserTest.username)
+    def testEventModel(self):
+        print("Testing Event Model")
+        sampleEventTest = Event.objects.get(title="Link up")
+        print("Sample Event Title => ",sampleEventTest.title)
     def testRsvpModel(self):
         print("Testing the Rsvp Model")
-        sampleEventTest = Rsvp.objects.get(name="sample")
+        sampleRsvpTest = Rsvp.objects.get(name="Faith")
+        print("Sample Rsvp from a user => ", sampleRsvpTest.name)
